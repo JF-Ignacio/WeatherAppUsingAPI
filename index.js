b18dd36527e2b44cd006c614870801e904c3d875
Franz Ignacio <franzignaciopogi@gmail.com>
2026-07-31 02:08:45 +0800

    Added API quotes, and enhance the UI in search section

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const apiKey = "b704f8087d6fbf2bc884c538e16ddf6e"; // FROM OPENWEATHER API
 const baseURL = "https://api.openweathermap.org/data/2.5/weather";
+
 let visits = parseInt(localStorage.getItem("searchVisits")) || 0;
 
 function getPosition() {
@@ -8,6 +9,42 @@ function getPosition() {
             resolve, reject
         );
     });
+}
+
+async function renderQuote(category) {
+    const qouteAPI = "fwl9nR0PRPo6ASZkMKcBmtKKzyTA4SJo7dPryZnO"; // FROM API NINJA
+    const qoute_base_url = "https://api.api-ninjas.com/v2/randomquotes";
+    const url = category ? `${qoute_base_url}?category=${encodeURIComponent(category)}` : qoute_base_url;
+
+    try {
+        const response = await fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    "X-API-Key": qouteAPI
+                }
+            }
+        );
+
+        if(!response.ok) {
+            throw new Error ("API FAILED: " + response.status);
+        }
+
+        const data = await response.json();
+        const [result] = data;
+
+        if(result) {
+            document.getElementById("quotes").textContent = result.quote;
+            document.getElementById("author").textContent = "- " + result.author;
+        }
+        else {
+            return;
+        }
+
+    }
+    catch(err) {
+        console.error("An error occurs", err);
+    }
 }
 
 function updateSearchCounts() {
@@ -19,7 +56,6 @@ function updateSearchCounts() {
 }
 
 async function getCurrentWeather() {
-
     const position = await getPosition();
     const {latitude, longitude} = position.coords;
     const data = await FetchWeatherByCoords(latitude, longitude);
@@ -50,11 +86,17 @@ async function FetchWeatherByCity(city) {
 }
 
 async function renderCurrentWeather(data) {
+
+    const tempImg = document.getElementById("temp-img");
+    const currentTemp = Math.round(data.main.temp);
+    tempImg.src = currentTemp >= 30 ? '/WeatherApp/assets/images/hot_temp.jpg' : '/WeatherApp/assets/images/cold_temp.jpg';
+
+
     document.getElementById("currentLocation").textContent = 
     `${data.name}, ${data.sys.country}`;
 
     document.getElementById("temperature").textContent = 
-    `${Math.round(data.main.temp)} * C`;
+    `${currentTemp} C`;
 
     document.getElementById("humidity").textContent =
     `${data.main.humidity}`;
@@ -66,43 +108,55 @@ async function renderCurrentWeather(data) {
     `${data.weather[0].description}`;
 
     const iconCode = data.weather[0].icon;
-    document.getElementById("weather-display").innerHTML =
-    `<img
-    src="https://openweathermap.org/img/wn/${iconCode}@2x.png"
-    alt="${data.weather[0].description}" 
-    style='width: 200px';
-    > `;
+    const currentWeatherIcon = document.getElementById("current-weather-icon");
+    currentWeatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    currentWeatherIcon.alt = data.weather[0].description;
 
 }
 
 async function renderSearchWeather(search) {
+    const searchTemp = Math.round(search.main.temp);
+    const searchTempImg = document.getElementById("search-temp-img");
+    searchTempImg.src = searchTemp >= 30 ? '/WeatherApp/assets/images/hot_temp.jpg' : '/WeatherApp/assets/images/cold_temp.jpg';
+
     document.getElementById("search-header-location").textContent =
     `${search.name} , ${search.sys.country}`;
 
     document.getElementById("search-temperature").textContent =
-    `TEMPERATURE:  ${search.main.temp}`;
+    `${searchTemp} C`;
 
     document.getElementById("search-humidity").textContent = 
-    `HUMIDITY: ${search.main.humidity}`;
+    `${search.main.humidity}`;
 
     document.getElementById("search-wind-speed").textContent =
-    `WIND SPEED: ${search.wind.speed}`;
+    `${search.wind.speed} m/s \n DEG: ${search.wind.deg}`;
 
     document.getElementById("search-weather-description").textContent =
     `${search.weather[0].description}`;
 
-    const searchIcon = data.weather[0].icon;
-    document.getElementById("search-weather-display").innerHTML =
-    `<img
-    src="https://openweathermap.org/img/wn/${searchIcon}@2x.png"
-    alt="${search.weather[0].description}";
-    > `;
+    const searchIcon = search.weather[0].icon;
+    const searchWeatherIcon = document.getElementById("search-weather-display");
+    searchWeatherIcon.src = `https://openweathermap.org/img/wn/${searchIcon}@2x.png`;
+    searchWeatherIcon.alt = search.weather[0].description;
+  
 }
 
 async function getWeather() {
     const citySearch = document.getElementById("search-location").value.trim();
+    const searchBtn = document.getElementById("search-btn");
+    const notice = document.getElementById("notice");
+
+    searchBtn.addEventListener("click", async () => {
+        if(citySearch === "") {
+            notice.textContent = "Empty Fields. Search first!";
+            return;
+        } 
+
+        notice.textContent = "";
+    })
 
     if(!citySearch) return;
+
 
     try {
         const data = await FetchWeatherByCity(citySearch);
@@ -124,4 +178,5 @@ async function getWeather() {
 window.addEventListener("DOMContentLoaded", () => {
     getCurrentWeather();
     updateSearchCounts();
+    renderQuote("happiness");
 });
